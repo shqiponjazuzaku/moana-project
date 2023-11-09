ddb8336f1968ba3c42baa163ebd25d9c59a4dc5d
Shqiponja Zuzaku <143807785+shqiponjazuzaku@users.noreply.github.com>
2023-11-09 15:15:34 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+getYear();
+
+$('.custom_slick_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    fade: true,
+    adaptiveHeight: true,
+    asNavFor: '.slick_slider_nav',
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            dots: false
+        }
+    }]
+})
+
+$('.slick_slider_nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.custom_slick_slider',
+    centerMode: false,
+    focusOnSelect: true,
+    variableWidth: true
+});
